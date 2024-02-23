71f6cd70b015d6127ee024bccfb0d45dc945353e
Aleksandr <aslyusarev@gmail.com>
2024-02-23 13:50:37 -0800

    styling MainActivity

--- app/src/main/java/com/example/beneapp/BeneficiaryDetails.kt
+++ app/src/main/java/com/example/beneapp/BeneficiaryDetails.kt
@@ -32,8 +32,6 @@ class BeneficiaryDetails : AppCompatActivity() {
 
         setContent {
             BeneAppTheme {
-                // A surface container using the 'background' color from the theme
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
--- app/src/main/java/com/example/beneapp/MainActivity.kt
+++ app/src/main/java/com/example/beneapp/MainActivity.kt
@@ -5,16 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.beneapp.ui.theme.BeneAppTheme
 import org.json.JSONArray
 import java.io.InputStreamReader
@@ -24,19 +42,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // reading/parsing json data
         val br = baseContext.assets.open("Beneficiaries.json").bufferedReader()
         val beneficiariesJson = br.use { it.readText() }
         val beneficiaries = JSONArray(beneficiariesJson)
 
         setContent {
             BeneAppTheme {
-                // A surface container using the 'background' color from the theme
+                Column(
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
                 ) {
-
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Beneficiaries",
+                            fontSize = 30.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
                     BeneList(beneficiaries)
                 }
             }
@@ -49,19 +78,56 @@ fun BeneList(beneficiaries: JSONArray, modifier: Modifier = Modifier) {
     val ctx = LocalContext.current
 
     LazyColumn {
+        // looping over beneficiaries in JSONArray
         items(beneficiaries.length()) { index ->
             val row = beneficiaries.getJSONObject(index)
             val firstName = row.getString("firstName")
             val lastName = row.getString("lastName")
+            val beneType = row.getString("beneType")
+            val designation = row.getString("designationCode")
 
+            // setting up intent to pass to BeneficiaryDetails activity
             val detailsIntent = Intent(ctx, BeneficiaryDetails::class.java)
             detailsIntent.putExtra("row", row.toString())
+            Card(modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize()
+                .clickable {
+                    ctx.startActivity(detailsIntent)
+                }
+            ) {
 
 
-            Text(
-                text = "$firstName $lastName",
-                modifier = Modifier.clickable { ctx.startActivity(detailsIntent) }
-            )
+                Text(
+                    text = "$firstName $lastName",
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "Type: $beneType",
+                    style = TextStyle(
+                        color = Color.DarkGray,
+                        fontSize = 16.sp,
+                    ),
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "Designation: ${
+                        when (designation) {
+                            "P" -> "Primary"
+                            "C" -> "Contingent"
+                            else -> ""
+                        }
+                    }",
+                    color = Color.DarkGray,
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+            }
+
+
         }
     }
 }
